fa84729979426e2135e81a5688ab5b091fbe23cb
Seungrok-J <seungrokjeong@gmail.com>
2024-10-04 15:17:43 +0900

    security1

--- springboot/src/main/java/org/boot/post_springboot/security/WebSecurityConfig.java
+++ springboot/src/main/java/org/boot/post_springboot/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package org.boot.post_springboot.security;
 
+import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionFixationProtectionStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
@@ -24,25 +28,38 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests((requests) -> requests
-                                .requestMatchers("/", "/home","/public/**").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                                .loginPage("/login")
-                                .permitAll()
-                )
-                .logout((logout) -> logout.permitAll())
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/", "/home", "/public/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll())
+                .logout(logout -> logout.permitAll())
                 .sessionManagement(management -> management
                         .sessionAuthenticationStrategy(sessionAuthenticationStrategy())
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                         .invalidSessionUrl("/login?invalid")
                         .maximumSessions(1)
                         .expiredUrl("/login?expired")
-                        .maxSessionsPreventsLogin(true)); // 세션 관리 전략 설정
+                        .maxSessionsPreventsLogin(true));
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // 모든 도메인에서의 접근을 허용
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용된 HTTP 메소드
+        configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
+        configuration.setAllowCredentials(true); // 크레덴셜 허용
+        configuration.setMaxAge(3600L); // 사전 요청(precache) 결과를 3600초 동안 캐시
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+        return source;
     }
 
     @Bean
