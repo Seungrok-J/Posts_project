aeed84fd52607966eb80fbe763d8ee8299c3cadf
Seungrok-J <seungrokjeong@gmail.com>
2024-10-04 15:07:48 +0900

    security

--- springboot/src/main/java/org/boot/post_springboot/security/WebSecurityConfig.java
+++ springboot/src/main/java/org/boot/post_springboot/security/WebSecurityConfig.java
@@ -1,5 +1,70 @@
 package org.boot.post_springboot.security;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionFixationProtectionStrategy;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
-    
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                                .requestMatchers("/", "/home","/public/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                                .loginPage("/login")
+                                .permitAll()
+                )
+                .logout((logout) -> logout.permitAll())
+                .sessionManagement(management -> management
+                        .sessionAuthenticationStrategy(sessionAuthenticationStrategy())
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .invalidSessionUrl("/login?invalid")
+                        .maximumSessions(1)
+                        .expiredUrl("/login?expired")
+                        .maxSessionsPreventsLogin(true)); // 세션 관리 전략 설정
+
+        return http.build();
+    }
+
+    @Bean
+    public SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        // 세션 고정 보호 전략 설정
+        return new SessionFixationProtectionStrategy();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserBuilder users = User.builder().passwordEncoder(passwordEncoder::encode);
+        UserDetails user = users
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
