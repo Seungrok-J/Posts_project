31b38852c646b67f4e671ebb67313e782dcbdb1a
Seungrok-J <seungrokjeong@gmail.com>
2024-10-07 07:05:44 +0900

    email

--- springboot/src/main/java/org/boot/post_springboot/demo/repository/UserRepository.java
+++ springboot/src/main/java/org/boot/post_springboot/demo/repository/UserRepository.java
@@ -3,15 +3,12 @@ package org.boot.post_springboot.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 import org.boot.post_springboot.demo.domain.User;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUserEmail(String userEmail);
-    Optional<User> findByUserId(Long userId);
-
-
+    boolean existexistsByNickName(String nickName);
+    boolean exiexistsByUserEmail(String userEmail);
 }
---
+++ springboot/src/main/java/org/boot/post_springboot/demo/service/EmailService.java
@@ -0,0 +1,7 @@
+package org.boot.post_springboot.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+}
