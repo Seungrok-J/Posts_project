19190b40c545cc8e07bdf50ed241ddf03fc2de75
SeungorkJeong <168049615+Seungrok-J@users.noreply.github.com>
2024-10-08 17:19:55 +0900

    Revert "Revert "코드 좀 봐줘여,,,,어케 하는건데 병합 내가 하도 당신 코드를 건들였어,, 도와줘,,""

--- springboot/src/main/java/org/boot/post_springboot/demo/repository/BoardsRepository.java
+++ springboot/src/main/java/org/boot/post_springboot/demo/repository/BoardsRepository.java
@@ -1,9 +1,17 @@
 package org.boot.post_springboot.demo.repository;
 
 import org.boot.post_springboot.demo.domain.Boards;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface BoardsRepository extends JpaRepository<Boards,Long>{
+
+    List<Boards> findAllByCategory_CateId(Long cateId);
+
+    List<Boards> findAllByUser_UserId(Long userId);
 }
