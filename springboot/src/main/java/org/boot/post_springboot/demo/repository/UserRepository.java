fcd4ab571cc0d8f7cc440c974d60f453ed5888dd
SeungorkJeong <168049615+Seungrok-J@users.noreply.github.com>
2024-10-08 17:04:17 +0900

    Revert "코드 좀 봐줘여,,,,어케 하는건데 병합 내가 하도 당신 코드를 건들였어,, 도와줘,,"

--- springboot/src/main/java/org/boot/post_springboot/demo/repository/UserRepository.java
+++ springboot/src/main/java/org/boot/post_springboot/demo/repository/UserRepository.java
@@ -6,14 +6,9 @@ import org.springframework.stereotype.Repository;
 
 import org.boot.post_springboot.demo.domain.User;
 
-import java.util.Optional;
-
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByNickName(String nickName);
-
     boolean existsByUserEmail(String userEmail);
-
-    Optional<User> findByUserName(String username); // 수정된 메서드 이름
 }
